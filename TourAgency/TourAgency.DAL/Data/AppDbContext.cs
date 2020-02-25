85b8f79340193cb2ad5df24e9ac13a79dcbdf721
Михаил Розпутний <mikhail.rozputnii@gmail.com>
2020-02-25 23:49:35 +0400

    create DB context

---
+++ TourAgency/TourAgency.DAL/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TourAgency.DAL.Data.Entities;
+
+namespace TourAgency.DAL.Data
+{
+    public class AppDbContext : IdentityDbContext<User, Role, int>
+    {
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderStatus> OrderStatuses { get; set; }
+        public DbSet<Tour> Tours { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+    }
+}
