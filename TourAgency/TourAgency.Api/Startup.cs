db9b2618a9e518e4f83c053608c6631e58265570
Михаил Розпутний <mikhail.rozputnii@gmail.com>
2020-02-26 16:19:26 +0400

    configure startup project

--- TourAgency/TourAgency.Api/Startup.cs
+++ TourAgency/TourAgency.Api/Startup.cs
@@ -1,15 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using TourAgency.DAL.Data;
+using TourAgency.DAL.Data.Entities;
+using TourAgency.DAL.Data.Repositories;
+using TourAgency.DAL.Data.Repositories.Implement;
 
 namespace TourAgency.Api
 {
@@ -25,6 +23,14 @@ namespace TourAgency.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options => options
+               .UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<User, Role>()
+               .AddEntityFrameworkStores<AppDbContext>();
+
+            services.AddScoped<IUnitOfWork, BaseUnitOfWork>();
+
             services.AddControllers();
         }
 
