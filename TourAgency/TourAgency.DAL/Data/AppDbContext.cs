08d55b0f37cf77ca442e37f6ca9fe58839ffbf9f
Михаил Розпутний <mikhail.rozputnii@gmail.com>
2020-03-08 22:56:20 +0200

    implement data context

--- TourAgency/TourAgency.DAL/Data/AppDbContext.cs
+++ TourAgency/TourAgency.DAL/Data/AppDbContext.cs
@@ -1,18 +1,36 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using TourAgency.DAL.Data.Configurations;
+using TourAgency.DAL.Data.Configurations.Identity;
 using TourAgency.DAL.Data.Entities;
 
 namespace TourAgency.DAL.Data
 {
     public class AppDbContext : IdentityDbContext<User, Role, int>
     {
+        public DbSet<Image> Images { get; set; }
         public DbSet<Location> Locations { get; set; }
         public DbSet<Order> Orders { get; set; }
         public DbSet<OrderStatus> OrderStatuses { get; set; }
         public DbSet<Tour> Tours { get; set; }
+        public DbSet<TourLocation> TourLocations { get; set; }
 
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+            modelBuilder.ApplyConfiguration(new RoleConfiguration());
+            modelBuilder.ApplyConfiguration(new UserRoleConfiguration());
+            modelBuilder.ApplyConfiguration(new ImageConfiguration());
+            modelBuilder.ApplyConfiguration(new LocationConfiguration());
+            modelBuilder.ApplyConfiguration(new OrderConfiguration());
+            modelBuilder.ApplyConfiguration(new OrderStatusConfiguration());
+            modelBuilder.ApplyConfiguration(new TourConfiguration());
+            modelBuilder.ApplyConfiguration(new TourLocationConfiguration());
         }
     }
 }
