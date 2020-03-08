eba7a78958ef6f8ced54ac794dbb1f43f4483aae
Михаил Розпутний <mikhail.rozputnii@gmail.com>
2020-03-08 22:55:49 +0200

    add base constatns

---
+++ TourAgency/TourAgency.DAL/Constants/Constants.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TourAgency.DAL.Constants
+{
+    public class Constants
+    {
+        public static class Roles
+        {
+            public const string Admin = "Admin";
+            public const string Customer = "Customer";
+        }
+
+        public enum OrderStatus
+        {
+            InProgress = 1,
+            Completed
+        }
+
+        public enum Role
+        {
+            Admin = 1,
+            Customer
+        }
+    }
+}
