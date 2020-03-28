3bdf4b9c1ee1588e1c0097383f83a7db920496d6
Михаил Розпутний <mikhail.rozputnii@gmail.com>
2020-03-28 09:27:37 +0200

    create view models

---
+++ TourAgency/TourAgency.Api/ViewModels/Tour/ImageViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace TourAgency.Api.ViewModels.Tour
+{
+    public class ImageViewModel
+    {
+        public int Id { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ TourAgency/TourAgency.Api/ViewModels/Tour/TourViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace TourAgency.Api.ViewModels.Tour
+{
+    public class TourViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+
+        public IEnumerable<LocationViewModel> LocationsViewModel { get; set; }
+        public IEnumerable<ImageViewModel> ImagesViewModel { get; set; }
+    }
+}
